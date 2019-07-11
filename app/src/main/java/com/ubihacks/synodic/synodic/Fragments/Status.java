126d007d1b90db9da9ba42f80a5a13918d8c14ef
ubaid <ubaidifti@gmail.com>
2019-07-11 18:58:46 +0500

    status

--- app/src/main/java/com/ubihacks/synodic/synodic/Fragments/Status.java
+++ app/src/main/java/com/ubihacks/synodic/synodic/Fragments/Status.java
@@ -1,109 +1,70 @@
 package com.ubihacks.synodic.synodic.Fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.ubihacks.synodic.synodic.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Status.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Status#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Status extends BaseFragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private TextView timeSleep;
+    private Button sleep;
+    private TextView timeDrive;
+    private Button drive;
+    private TextView timeOffline;
+    private Button offline;
+    private TextView timeOnline;
+    private Button online;
+    private TextView driving;
+    private TextView shift;
+    private TextView cycle;
+    private BarChart chart1;
+    private SeekBar seekBar2;
+    private SeekBar seekBar1;
+    private TextView tvXMax;
+    private TextView tvYMax;
 
     public Status() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Status.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Status newInstance(String param1, String param2) {
-        Status fragment = new Status();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_status, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_status, container, false);
+        initView(view);
+
+        return view;
+
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    private void initView(View view) {
+        timeSleep = (TextView) view.findViewById(R.id.timeSleep);
+        sleep = (Button) view.findViewById(R.id.sleep);
+        timeDrive = (TextView) view.findViewById(R.id.timeDrive);
+        drive = (Button) view.findViewById(R.id.drive);
+        timeOffline = (TextView) view.findViewById(R.id.timeOffline);
+        offline = (Button) view.findViewById(R.id.offline);
+        timeOnline = (TextView) view.findViewById(R.id.timeOnline);
+        online = (Button) view.findViewById(R.id.online);
+        driving = (TextView) view.findViewById(R.id.driving);
+        shift = (TextView) view.findViewById(R.id.shift);
+        cycle = (TextView) view.findViewById(R.id.cycle);
+        chart1 = (BarChart) view.findViewById(R.id.chart1);
+        seekBar2 = (SeekBar) view.findViewById(R.id.seekBar2);
+        seekBar1 = (SeekBar) view.findViewById(R.id.seekBar1);
+        tvXMax = (TextView) view.findViewById(R.id.tvXMax);
+        tvYMax = (TextView) view.findViewById(R.id.tvYMax);
     }
 }
