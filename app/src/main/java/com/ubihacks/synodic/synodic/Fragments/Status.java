234bbf169b7bc7a2f984be794cefe29570d3ba16
ubaid <ubaidifti@gmail.com>
2019-07-11 12:10:41 +0500

    initial

--- app/src/main/java/com/ubihacks/synodic/synodic/Fragments/Status.java
+++ app/src/main/java/com/ubihacks/synodic/synodic/Fragments/Status.java
@@ -18,7 +18,7 @@ import com.ubihacks.synodic.synodic.R;
  * Use the {@link Status#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Status extends Fragment {
+public class Status extends BaseFragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
