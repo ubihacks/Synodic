19b9e649e431f8ba98f2726de4cb188665428fa5
obaid-ur-rehman <obaidpunjab@gmail.com>
2019-07-15 15:10:19 +0500

    Added Signature Fragment

---
+++ app/src/main/java/com/ubihacks/synodic/synodic/Signature.java
@@ -0,0 +1,29 @@
+package com.ubihacks.synodic.synodic;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Signature extends Fragment {
+
+
+    public Signature() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signature, container, false);
+    }
+
+}
